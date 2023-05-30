ce8815accb0ed8e0fa87c11a88bce29abacd66fc
Vadim Shaibe <vshaibe@informationsfabrik.de>
2023-05-30 15:20:54 +0200

    removed variables

--- app/config/authConfig.ts
+++ app/config/authConfig.ts
@@ -2,8 +2,8 @@ import { Configuration, RedirectRequest } from "@azure/msal-browser";
 
 export const msalConfig: Configuration = {
   auth: {
-    clientId: `${process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID}`,
-    authority: `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID}`,
+    clientId: `957259b3-2460-4507-bf3d-483c21495ea4`,
+    authority: `https://login.microsoftonline.com/61fb942c-df9d-475d-ba36-f0f3f7701c30`,
     redirectUri: "/",
   },
 };
